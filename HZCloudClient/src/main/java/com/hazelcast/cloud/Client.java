1039133bf68db37dd5928e98edcf533624424802
Vasundar <vasundar.lakkalapudi@hazelcast.com>
2021-07-21 15:39:25 +0530

    simple client code to bench mark

--- HZCloudClient/src/main/java/com/hazelcast/cloud/Client.java
+++ HZCloudClient/src/main/java/com/hazelcast/cloud/Client.java
@@ -17,29 +17,58 @@ import static com.hazelcast.client.properties.ClientProperty.STATISTICS_ENABLED;
  * See: <a href="https://docs.cloud.hazelcast.com/docs/java-client">https://docs.cloud.hazelcast.com/docs/java-client</a>
  */
 public class Client {
-
     public static void main(String[] args) {
-        ClientConfig config = new ClientConfig();
-        config.setProperty(STATISTICS_ENABLED.getName(), "true");
-        config.setProperty(HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "jDRIVHfDcZ34VoNtutyaWXRvU8W8hkYHIshTt2TJN1dVUXS50f");
-        config.setProperty("hazelcast.client.cloud.url", "https://uat.hazelcast.cloud");
-        config.setClusterName("vasundar-test1");
-        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
-
-        System.out.println("Connection Successful!");
-        System.out.println("Now the map named 'map' will be filled with random entries.");
-
-        IMap<String, String> map = client.getMap("map");
-        Random random = new Random();
-        int iterationCounter = 0;
-        while (true) {
-            int randomKey = random.nextInt(100_000);
-            map.put("key-" + randomKey, "value-" + randomKey);
-            map.get("key-" + random.nextInt(100_000));
-            if (++iterationCounter == 10) {
-                iterationCounter = 0;
-                System.out.println("Current map size: " + map.size());
-            }
+        int batch = 100000;
+        for (int i = 0; i < 50; i++) {
+            MyThread thread = new MyThread(i * batch);
+            thread.setDaemon(true);
+            thread.start();
         }
     }
 }
+
+
+class MyThread extends Thread {
+    private int value;
+    HazelcastInstance client;
+    Random random = new Random();
+    MyThread(int value) {
+        this.value = value;
+        ClientConfig config = new ClientConfig();
+        config.setProperty(STATISTICS_ENABLED.getName(), "true");
+        config.setProperty(HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "2B6qs5q3NCs8FGvBe6N2jmS3wpfrcGw1w2yGRFK54gKczwbHOk");
+        config.setProperty("hazelcast.client.cloud.url", "https://uat.hazelcast.cloud");
+        config.setClusterName("twonode");
+        client = HazelcastClient.newHazelcastClient(config);
+        System.out.println("Connection Successful!  " + Thread.currentThread().getName());
+    }
+    public void run() {
+        IMap<String, String> map = client.getMap("mapTwo");
+        long tt = 0;
+        long gtt = 0;
+        long totalCount = 0;
+        long iterCount = 0;
+
+        while(true) {
+            int rkey = value + random.nextInt(100_000);
+            long stp = System.currentTimeMillis();
+            map.put("key-" +rkey, "value-" +rkey);
+            long ftp = System.currentTimeMillis();
+            long stg = System.currentTimeMillis();
+            map.get("key-" + (value +  random.nextInt(100_000)));
+            long ftg = System.currentTimeMillis();
+            if(iterCount == 1000) {
+                iterCount = 0;
+                System.out.println("Current Map size : " + map.size());
+            }
+            totalCount++;
+            long diff = ftp - stp;
+            tt += diff;
+            System.out.println("put average is :" + (float)tt/totalCount);
+            long gdiff = ftg - stg;
+            gtt += gdiff;
+            System.out.println("get average is " + (float)gtt/totalCount);
+        }
+    }
+}
+
