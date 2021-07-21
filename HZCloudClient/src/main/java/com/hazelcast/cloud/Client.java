b32814e958ff295dcd24250a64e84b593477dc85
Vasundar <vasundar.lakkalapudi@hazelcast.com>
2021-07-21 15:27:11 +0530

    pushing hazelcast client code without changes

---
+++ HZCloudClient/src/main/java/com/hazelcast/cloud/Client.java
@@ -0,0 +1,45 @@
+package com.hazelcast.cloud;
+
+import java.util.Random;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+import static com.hazelcast.client.properties.ClientProperty.HAZELCAST_CLOUD_DISCOVERY_TOKEN;
+import static com.hazelcast.client.properties.ClientProperty.STATISTICS_ENABLED;
+
+/**
+ * This is boilerplate application that configures client to connect Hazelcast Cloud cluster.
+ * After successful connection, it puts random entries into the map.
+ * <p>
+ * See: <a href="https://docs.cloud.hazelcast.com/docs/java-client">https://docs.cloud.hazelcast.com/docs/java-client</a>
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        ClientConfig config = new ClientConfig();
+        config.setProperty(STATISTICS_ENABLED.getName(), "true");
+        config.setProperty(HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "jDRIVHfDcZ34VoNtutyaWXRvU8W8hkYHIshTt2TJN1dVUXS50f");
+        config.setProperty("hazelcast.client.cloud.url", "https://uat.hazelcast.cloud");
+        config.setClusterName("vasundar-test1");
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
+
+        System.out.println("Connection Successful!");
+        System.out.println("Now the map named 'map' will be filled with random entries.");
+
+        IMap<String, String> map = client.getMap("map");
+        Random random = new Random();
+        int iterationCounter = 0;
+        while (true) {
+            int randomKey = random.nextInt(100_000);
+            map.put("key-" + randomKey, "value-" + randomKey);
+            map.get("key-" + random.nextInt(100_000));
+            if (++iterationCounter == 10) {
+                iterationCounter = 0;
+                System.out.println("Current map size: " + map.size());
+            }
+        }
+    }
+}
